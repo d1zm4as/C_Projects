8b6524a7fb3fda2921494b74525892b8aef1cd26
d1zm4as <pedrofamilia364@gmail.com>
2024-04-08 23:28:26 -0300

    arrow

--- UI/ncurses2.c
+++ UI/ncurses2.c
@@ -4,4 +4,25 @@
 
 int main(){
     
+    int x,y;
+
+    initscr();
+    clear();
+    // gets the coordinates of the cursor
+    getyx(stdscr,y,x);
+    printw( "x = %d\n"
+            "y = %d"
+    );
+
+    refresh();
+    y = 5;
+    x = 10;
+    //moves the cursor
+    move(y,x)   ;
+    printw("Over here!");
+    refresh();
+    getch();
+    endwin();
+    return 0;
+
 }
