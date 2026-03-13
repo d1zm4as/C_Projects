e61d0470290e94cea5dddc27616040441b2a1cec
d1zm4as <pedrofamilia364@gmail.com>
2026-03-13 12:50:57 -0300

    +firstCircle

--- Ray Tracing/main.c
+++ Ray Tracing/main.c
@@ -1,20 +1,47 @@
 #include <stdio.h>
-
+#include <math.h>
 #include <SDL2/SDL.h> 
 
 #define WIDTH 900
 
+#define COLOR_WHITE 0xffffffff
 #define HEIGHT 600
 
-#define COLOR_WHITE 0xffffffff
+struct Circle
+{
+    double x;
+    double y;
+    double r;
+    /* data */
+};
+
+void FillCircle(SDL_Surface* surface, struct Circle circle, Uint32 color){
+    double radius_squared = pow(circle.r,2);
+    for (double x = circle.x-circle.r;x<=circle.x+circle.r;x++){
+        for (double y = circle.y-circle.r;y<=circle.y+circle.r;y++){
+            double distance_squared = pow(x-circle.x,2)+pow(y-circle.y,2);
+            if (distance_squared < radius_squared){
+                SDL_Rect pixel = (SDL_Rect){x,y,1,1};
+                SDL_FillRect(surface, &pixel, color);
+            }
+    }
+}
+
+}
+
+
+
 int main(){
 
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* window = SDL_CreateWindow("Raytracing",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT,0);
     SDL_Surface* surface = SDL_GetWindowSurface(window);
-    SDL_Rect rect = (SDL_Rect) {200,200,200,200};
-    SDL_FillRect(surface, &rect, COLOR_WHITE);
+    // SDL_Rect rect = (SDL_Rect) {200,200,200,200};
+    // SDL_FillRect(surface, &rect, COLOR_WHITE);
+    struct Circle circle = {200,200,80};
+    
+    FillCircle(surface, circle,COLOR_WHITE);
     SDL_UpdateWindowSurface(window);
-    SDL_Delay(500);
+    SDL_Delay(5000);
     return 0;
 }
