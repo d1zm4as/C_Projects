a9ce44a8c93b7256045d3fe3bbb890513309bda6
d1zm4as <pedrofamilia364@gmail.com>
2026-03-13 13:35:38 -0300

    +movingCircle

--- Ray Tracing/main.c
+++ Ray Tracing/main.c
@@ -3,9 +3,10 @@
 #include <SDL2/SDL.h> 
 
 #define WIDTH 900
+#define HEIGHT 600
 
 #define COLOR_WHITE 0xffffffff
-#define HEIGHT 600
+#define COLOR_BLACK 0x00000000
 
 struct Circle
 {
@@ -36,12 +37,33 @@ int main(){
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* window = SDL_CreateWindow("Raytracing",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT,0);
     SDL_Surface* surface = SDL_GetWindowSurface(window);
-    // SDL_Rect rect = (SDL_Rect) {200,200,200,200};
-    // SDL_FillRect(surface, &rect, COLOR_WHITE);
-    struct Circle circle = {200,200,80};
     
-    FillCircle(surface, circle,COLOR_WHITE);
-    SDL_UpdateWindowSurface(window);
-    SDL_Delay(5000);
+    struct Circle circle = {200,200,80};
+    SDL_Rect erase_rect = (SDL_Rect){0,0,WIDTH,HEIGHT};
+    int simulation_running = 1;
+    
+    // FillCircle(surface, circle,COLOR_WHITE);
+    SDL_Event event;
+    
+    while(simulation_running){
+        while(SDL_PollEvent(&event)){
+            if (event.type ==SDL_QUIT){
+                simulation_running = 0;
+            }
+            if (event.type == SDL_MOUSEMOTION && event.motion.state !=0){
+                circle.x = event.motion.x;
+                circle.y = event.motion.y;
+
+            }
+        }
+        SDL_FillRect(surface, &erase_rect, COLOR_BLACK);
+        FillCircle(surface, circle, COLOR_WHITE);
+        SDL_UpdateWindowSurface(window);
+        SDL_Delay(10);
+
+    }
+
+
+
     return 0;
 }
